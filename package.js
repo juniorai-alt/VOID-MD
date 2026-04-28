7b95cb6dcba9486babbed3cc709c3e9dcd54d565
juniorai-alt <nyanugajunior@gmail.com>
2026-04-28 20:17:50 +0300

    Initialize package.json with project metadata

---
+++ package.js
@@ -0,0 +1,15 @@
+{
+  "name": "unborn-bot",
+  "version": "1.0.0",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "@whiskeysockets/baileys": "latest",
+    "@hapi/boom": "^10.0.1",
+    "pino": "^8.17.2",
+    "qrcode": "^1.5.3",
+    "express": "^4.18.2"
+  }
+}
